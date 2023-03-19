6889f34359268874b32e24a772266bc5d1a0b8a9
HyunJin Kim <hyunnjinn@gmail.com>
2023-03-19 14:28:43 +0900

    [Bronze V] Title: 합, Time: 204 ms, Memory: 17676 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a;
+        a = scanner.nextInt();
+        System.out.println(a * (a + 1) / 2);
+    }
+}
