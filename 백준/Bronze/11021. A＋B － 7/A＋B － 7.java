78dd183793d367c47b73017c317b814e057c7a4d
HyunJin Kim <hyunnjinn@gmail.com>
2023-03-19 14:50:23 +0900

    [Bronze V] Title: A+B - 7, Time: 228 ms, Memory: 17936 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a, b, c;
+        a = scanner.nextInt();
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 1 ; i <= a ; i++) {
+            b = scanner.nextInt();
+            c = scanner.nextInt();
+            result.append("Case #").append(i).append(": ").append((b + c));
+            if (i != a) {
+                result.append("\n");
+            }
+        }
+        System.out.println(result);
+    }
+}
