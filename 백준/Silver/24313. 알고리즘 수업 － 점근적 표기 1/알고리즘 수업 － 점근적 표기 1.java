b5a560a8b368259a0d4df4d1e5e231432285299a
HyunJin Kim <hyunnjinn@gmail.com>
2023-05-28 22:48:30 +0900

    [Silver V] Title: 알고리즘 수업 - 점근적 표기 1, Time: 208 ms, Memory: 17672 KB -BaekjoonHub

---
+++ 백준/Silver/24313. 알고리즘 수업 － 점근적 표기 1/알고리즘 수업 － 점근적 표기 1.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a1 = scanner.nextInt();
+        int a0 = scanner.nextInt();
+        int c = scanner.nextInt();
+        int n0 = scanner.nextInt();
+        if (a1 * n0 + a0 <= c * n0 && a1 <= c) {
+            System.out.println(1);
+        } else {
+            System.out.println(0);
+        }
+    }
+}
