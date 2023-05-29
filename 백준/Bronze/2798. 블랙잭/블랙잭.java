3fb5c3987e88655238e0ffd3f1e1a43b17121e4d
HyunJin Kim <hyunnjinn@gmail.com>
2023-05-29 12:45:55 +0900

    [Bronze II] Title: 블랙잭, Time: 220 ms, Memory: 17656 KB -BaekjoonHub

---
+++ 백준/Bronze/2798. 블랙잭/블랙잭.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int m = scanner.nextInt();
+        int[] nums = new int[n];
+        for (int i = 0; i < n; i++) {
+            nums[i] = scanner.nextInt();
+        }
+        int result = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                for (int k = j + 1; k < n; k++) {
+                    int sum = nums[i] + nums[j] + nums[k];
+                    if (sum <= m && result < sum) {
+                        result = sum;
+                    }
+                }
+            }
+        }
+        System.out.println(result);
+    }
+}
