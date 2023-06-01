e60e4467c234dc4a652f23bd137ab8a255c72c32
HyunJin Kim <hyunnjinn@gmail.com>
2023-06-01 21:07:56 +0900

    [Bronze II] Title: 수 정렬하기, Time: 364 ms, Memory: 22544 KB -BaekjoonHub

---
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        Arrays.stream(arr).sorted().forEach(System.out::println);
+    }
+}
