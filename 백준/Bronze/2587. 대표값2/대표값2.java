d546612dc23b5c81e34040d5f4565345b46c3ad0
HyunJin Kim <hyunnjinn@gmail.com>
2023-06-01 21:14:30 +0900

    [Bronze II] Title: 대표값2, Time: 216 ms, Memory: 18036 KB -BaekjoonHub

---
+++ 백준/Bronze/2587. 대표값2/대표값2.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] arr = new int[5];
+        for (int i = 0; i < 5; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        arr = Arrays.stream(arr).sorted().toArray();
+        System.out.println((int) Arrays.stream(arr).average().orElse(0));
+        System.out.println(arr[2]);
+    }
+}
