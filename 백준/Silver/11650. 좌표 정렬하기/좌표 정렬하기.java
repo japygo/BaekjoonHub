c78f20e27c7456be090073964bb333b6eab3bd7d
HyunJin Kim <hyunnjinn@gmail.com>
2023-06-03 00:23:13 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 2180 ms, Memory: 90304 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(bufferedReader.readLine());
+        int[][] arr = new int[n][];
+        for (int i = 0 ; i < n; i++) {
+            arr[i] = Arrays.stream(bufferedReader.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        }
+        Arrays.stream(arr).sorted((o1, o2) -> {
+            if (o1[0] != o2[0]) {
+                return o1[0] - o2[0];
+            }
+            return o1[1] - o2[1];
+        }).map(ints -> ints[0] + " " + ints[1]).forEach(System.out::println);
+    }
+}
