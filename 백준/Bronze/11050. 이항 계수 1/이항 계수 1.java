1e3f09ec7a53bf0d57f91b146a72f98cef3f7da5
HyunJin Kim <hyunnjinn@gmail.com>
2023-06-19 21:15:43 +0900

    [Bronze I] Title: 이항 계수 1, Time: 124 ms, Memory: 14156 KB -BaekjoonHub

---
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+        int n = Integer.parseInt(stringTokenizer.nextToken());
+        int k = Integer.parseInt(stringTokenizer.nextToken());
+        System.out.println(factorial(n) / (factorial(k) * factorial(n - k)));
+    }
+    public static int factorial(int n) {
+        if (n == 0) {
+            return 1;
+        }
+        return n * factorial(n - 1);
+    }
+}
