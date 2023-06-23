73cc38a054f4f93da84528820ec0858da617b432
HyunJin Kim <hyunnjinn@gmail.com>
2023-06-23 23:26:48 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 124 ms, Memory: 14112 KB -BaekjoonHub

---
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(bufferedReader.readLine());
+        System.out.println(fibonacci(n));
+    }
+    public static long fibonacci(int n) {
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+}
