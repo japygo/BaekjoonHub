de88229e8379b86016b8965900f6192fec407b10
HyunJin Kim <hyunnjinn@gmail.com>
2023-06-23 23:24:08 +0900

    [Bronze V] Title: 팩토리얼 2, Time: 132 ms, Memory: 14164 KB -BaekjoonHub

---
+++ 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(bufferedReader.readLine());
+        System.out.println(factorial(n));
+    }
+    public static long factorial(int n) {
+        if (n == 0) {
+            return 1;
+        }
+        return n * factorial(n - 1);
+    }
+}
