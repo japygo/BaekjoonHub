0f3df3ccecb9bd949dc60c8c1e1ffec9d70e6e29
HyunJin Kim <hyunnjinn@gmail.com>
2023-07-01 22:16:28 +0900

    [level 1] Title: 폰켓몬, Time: 2.98 ms, Memory: 77.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.java
@@ -0,0 +1,16 @@
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution {
+    public int solution(int[] nums) {
+        int answer = nums.length / 2;
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            set.add(num);
+        }
+        if (set.size() < answer) {
+            answer = set.size();
+        }
+        return answer;
+    }
+}
