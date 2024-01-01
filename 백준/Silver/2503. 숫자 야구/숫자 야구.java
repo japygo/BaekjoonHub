946aa6220e0705e5f7ce0b45837ff52cc02146f2
HyunJin Kim <hyunnjinn@gmail.com>
2024-01-02 00:02:18 +0900

    [Silver III] Title: 숫자 야구, Time: 232 ms, Memory: 18444 KB -BaekjoonHub

--- 백준/Silver/2503. 숫자 야구/숫자 야구.java
+++ 백준/Silver/2503. 숫자 야구/숫자 야구.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
 public class Main {
+    static int result = 0;
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int N = scanner.nextInt();
@@ -13,57 +14,65 @@ public class Main {
             hints[i][1] = strike;
             hints[i][2] = ball;
         }
+        recur(0, 100, hints, N);
+        System.out.println(result);
+    }
 
-        int count = 0;
-        for (int i = 1; i <= 9; i++) {
-            for (int j = 1; j <= 9; j++) {
-                for (int k = 1; k <= 9; k++) {
-                    if (i == j || j == k || k == i) {
-                        continue;
-                    }
-
-                    int cnt = 0;
-                    for (int[] hint : hints) {
-                        int num = hint[0];
-                        int strike = hint[1];
-                        int ball = hint[2];
-                        int strikeCnt = 0;
-                        int ballCnt = 0;
-
-                        int first = num / 100;
-                        int second = num % 100 / 10;
-                        int third = num % 100 % 10;
-
-                        if (i == first) {
-                            strikeCnt++;
-                        }
-                        if (j == second) {
-                            strikeCnt++;
-                        }
-                        if (k == third) {
-                            strikeCnt++;
-                        }
-                        if (i == second || i == third) {
-                            ballCnt++;
-                        }
-                        if (j == third || j == first) {
-                            ballCnt++;
-                        }
-                        if (k == first || k == second) {
-                            ballCnt++;
-                        }
-
-                        if (ball == ballCnt && strike == strikeCnt) {
-                            cnt++;
-                        }
-                    }
-
-                    if (cnt == N) {
-                        count++;
-                    }
-                }
-            }
+    public static void recur(int index, int number, int[][] hints, int N) {
+        if (number == 1000) {
+            return;
         }
-        System.out.println(count);
+
+        if (index == N) {
+            result += 1;
+            recur(0, number + 1, hints, N);
+            return;
+        }
+
+        int i = number / 100;
+        int j = number % 100 / 10;
+        int k = number % 100 % 10;
+
+        if (i == j || j == k || k == i ||
+                i == 0 || j == 0 || k == 0) {
+            recur(0, number + 1, hints, N);
+            return;
+        }
+
+        int[] hint = hints[index];
+        int num = hint[0];
+        int strike = hint[1];
+        int ball = hint[2];
+        int strikeCnt = 0;
+        int ballCnt = 0;
+
+        int first = num / 100;
+        int second = num % 100 / 10;
+        int third = num % 100 % 10;
+
+        if (i == first) {
+            strikeCnt++;
+        }
+        if (j == second) {
+            strikeCnt++;
+        }
+        if (k == third) {
+            strikeCnt++;
+        }
+        if (i == second || i == third) {
+            ballCnt++;
+        }
+        if (j == third || j == first) {
+            ballCnt++;
+        }
+        if (k == first || k == second) {
+            ballCnt++;
+        }
+
+        if (ball == ballCnt && strike == strikeCnt) {
+            recur(index + 1, number, hints, N);
+        } else {
+            recur(0, number + 1, hints, N);
+        }
     }
 }
