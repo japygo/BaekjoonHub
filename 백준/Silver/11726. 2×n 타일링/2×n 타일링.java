78dd73e38bcd50c10b3ee434c8c89c91e1088cdf
HyunJin Kim <hyunnjinn@gmail.com>
2024-01-02 16:53:08 +0900

    [Silver III] Title: 2×n 타일링, Time: 212 ms, Memory: 17784 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        if (n == 1) {
+            System.out.println(1);
+            return;
+        }
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            dp[i] = (dp[i - 2] + dp[i - 1]) % 10_007;
+        }
+        System.out.println(dp[n]);
+    }
+}
