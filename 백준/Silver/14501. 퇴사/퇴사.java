9f1cccf6e960184540cc5770314a70a22de4ca8f
HyunJin Kim <hyunnjinn@gmail.com>
2024-01-02 14:49:24 +0900

    [Silver III] Title: 퇴사, Time: 208 ms, Memory: 17740 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.java
+++ 백준/Silver/14501. 퇴사/퇴사.java
@@ -1,30 +1,39 @@
 import java.util.Scanner;
 
 public class Main {
-    static int result;
+    static int result = Integer.MIN_VALUE;
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int N = scanner.nextInt();
         int[][] arr = new int[N][2];
+        int[] dp = new int[N];
         for (int i = 0; i < N; i++) {
             arr[i][0] = scanner.nextInt();
             arr[i][1] = scanner.nextInt();
+            dp[i] = Integer.MIN_VALUE;
         }
-        recur(0, 0, arr, N);
+        recur(0, arr, N, dp);
+        for (int i = 0; i < N; i++) {
+            result = Math.max(result, dp[i]);
+        }
         System.out.println(result);
     }
 
-    public static void recur(int index, int price, int[][] arr, int N) {
+    public static int recur(int index, int[][] arr, int N, int[] dp) {
         if (index == N) {
-            result = Math.max(result, price);
-            return;
+            return 0;
         }
 
         if (index > N) {
-            return;
+            return Integer.MIN_VALUE;
         }
 
-        recur(index + arr[index][0], price + arr[index][1], arr, N);
-        recur(index + 1, price, arr, N);
+        if (dp[index] != Integer.MIN_VALUE) {
+            return dp[index];
+        }
+
+        dp[index] = Math.max(recur(index + arr[index][0], arr, N, dp) + arr[index][1], recur(index + 1, arr, N, dp));
+
+        return dp[index];
     }
 }
