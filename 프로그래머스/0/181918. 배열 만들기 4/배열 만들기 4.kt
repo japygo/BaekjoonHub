f8ae70edb92fb691721b5e6df4208ae215f1b5d6
HyunJin Kim <hyunnjinn@gmail.com>
2025-12-18 22:06:55 +0900

    [level 0] Title: 배열 만들기 4, Time: 24.87 ms, Memory: 70 MB -BaekjoonHub

---
+++ 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.kt
@@ -0,0 +1,22 @@
+class Solution {
+    fun solution(arr: IntArray): IntArray {
+        var stk = mutableListOf<Int>()
+        var i = 0
+        
+        while (i < arr.size) {
+            if (stk.isEmpty()) {
+                stk.add(arr[i])
+                i += 1
+            } else {
+                if (stk.last() < arr[i]) {
+                    stk.add(arr[i])
+                    i += 1
+                } else {
+                    stk.removeLast()
+                }
+            }
+        }
+        
+        return stk.toIntArray()
+    }
+}
