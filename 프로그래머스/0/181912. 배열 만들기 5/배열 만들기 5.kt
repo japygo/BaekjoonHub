c5ef2124777c54c93426aa0fc283199cc664ea7c
HyunJin Kim <hyunnjinn@gmail.com>
2025-12-20 16:51:08 +0900

    [level 0] Title: 배열 만들기 5, Time: 15.50 ms, Memory: 76.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181912. 배열 만들기 5/배열 만들기 5.kt
@@ -0,0 +1,5 @@
+class Solution {
+    fun solution(intStrs: Array<String>, k: Int, s: Int, l: Int): IntArray {
+        return intStrs.map { it.substring(s, s + l).toInt() }.filter { it > k }.toIntArray()
+    }
+}
