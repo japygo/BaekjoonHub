c902e6c20a94e7fdf446865a215a035792106172
HyunJin Kim <hyunnjinn@gmail.com>
2025-12-28 13:00:58 +0900

    [level 0] Title: 1로 만들기, Time: 0.04 ms, Memory: 62.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181880. 1로 만들기/1로 만들기.kt
@@ -0,0 +1,19 @@
+class Solution {
+    fun solution(num_list: IntArray): Int {
+        var answer: Int = 0
+        
+        for (num in num_list) {
+            var n = num
+            while (n != 1) {
+                answer += 1
+                if (n % 2 == 0) {
+                    n /= 2
+                } else {
+                    n = (n - 1) / 2
+                }
+            }
+        }
+        
+        return answer
+    }
+}
